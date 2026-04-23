f284adf4d97e89bd674d101e72115aa13788759d
Livia <pequenobiscooito@gmail.com>
2026-04-23 10:56:52 -0300

    sua mensagem aqui

---
+++ estacao-meteorologica-front/src/App.jsx
@@ -0,0 +1,16 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import './css/auth.css';
+import './css/variables.css';
+import  Rotas from './routes';
+
+function App() {
+
+  return (
+    <>
+        <Rotas/>
+    </>
+  )
+}
+
+export default App
